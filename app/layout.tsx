2dc83b6aee2c7db90763d0652227d7241ab9ddd0
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-05-04 04:31:40 +0700

    feat:add footer, center section 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/app/ui/navbar/navbar";
-
+import Footer from "@/app/ui/footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -26,6 +26,7 @@ export default function RootLayout({
       <body className={inter.className}>
       <div className="fixed top-0 left-0 right-0"><Navbar/></div>
         {children}
+        <Footer />
       </body>
     </html>
   );
