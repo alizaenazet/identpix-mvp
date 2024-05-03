8b90b92920319705ca6095af4d8716944d15ebcc
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-05-03 19:35:11 +0700

    feat:add register form

---
+++ app/register/page.tsx
@@ -0,0 +1,16 @@
+import RegisterForm from "@/app/ui/register/register-form";
+export default function Page() {
+  return (
+    <main className='flex h-screen flex-col  justify-center items-center p-16'>
+        <div className="flex flex-col items-start justify-center gap-3.5 md:w-[416px]" >
+            <div className="flex flex-col items-start justify-center gap-2">
+            <h1 className="text-xl font-semibold">Everyone has a purpose</h1>
+            <h3 className="text-neutral-400 text-xl font-semibold">Register Your Role</h3>
+            </div>
+            <div className="flex flex-col items-start gap-3.5 w-full">
+                <RegisterForm />
+            </div>
+        </div>
+    </main>
+  )
+}
---
+++ app/ui/register/register-form.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+
+export default function RegisterForm() {
+  return (
+    <form action="/" className="gap-y-3 md: gap-x-3 flex flex-col w-full ">
+        <div className="gap-y-3 md: gap-x-3 flex flex-col md:flex-row w-full">
+            <div className="flex flex-1 flex-col gap-1">
+                <Label>Backround</Label>
+                <select 
+                name="" 
+                id=""  
+                className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                >
+                <option key="individual" value="individual" selected>
+                    photographer
+                </option>
+                <option key="free-lancer" value="free-lancer">
+                    freelancer
+                </option>
+                <option key="student" value="student">
+                    student
+                </option>
+                </select>
+            </div>
+            <div className="flex flex-1 flex-col gap-1">
+                <Label>Choose plan</Label>
+                <select 
+                name="" 
+                id=""
+                className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                >
+                <option value="" selected>
+                    individual
+                </option>
+                <option value="">
+                    small team
+                </option>
+                <option value="">
+                    organization
+                </option>
+
+                </select>
+            </div>
+        </div>
+        <Button className="w-full">Continue</Button>
+    </form>
+  )
+}
