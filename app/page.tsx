d5b3297c28ef98cd4b00f2cb633688f6e6c934f3
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-05-03 09:56:48 +0700

    init project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="w-full h-full flex flex-col items-center bg-red-400">
+        <h1>Identpix MVP</h1>
+        <h4>pijaraya team</h4>
+      </div>    
+    </main>
+  );
+}
