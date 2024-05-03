954b9e2fd8d27c46fee4c79283977546ceaa3d2d
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-05-03 20:04:35 +0700

    feat: add coming-soong page view

---
+++ app/ui/coming-soon/email-form.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+
+export default function EmailForm() {
+  return (
+    <form className="flex flex-col gap-y-2">
+        <Label className="md:text-base md:font-semibold">Get our newest info</Label>
+        <Input type="email" placeholder="Email" />
+        <Button className="w-24 ">Continue</Button>
+    </form>
+  )
+}
