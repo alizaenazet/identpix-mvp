dfdbf97c44969886784b418778602394fd0bcd98
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-05-06 03:36:31 +0700

    refactor: change to notify button only

--- app/coming-soon/page.tsx
+++ app/coming-soon/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
                 Coming Soon !
             </h1>
             <p className='text-sm font-medium w-full md:min-w-[446px]  md:text-base'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+              We will available soon, so look forward to an enjoyable firsthand experience with us.
             </p>
             <EmailForm />
         </div>
--- app/ui/coming-soon/email-form.tsx
+++ app/ui/coming-soon/email-form.tsx
@@ -5,8 +5,6 @@ import { Input } from "@/components/ui/input"
 export default function EmailForm() {
   return (
     <form className="flex flex-col gap-y-2">
-        <Label className="md:text-base md:font-semibold">Get our newest info</Label>
-        <Input type="email" placeholder="Email" />
         <Button className="w-24 ">Continue</Button>
     </form>
   )
