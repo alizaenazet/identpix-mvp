7dd925531cda33cab262a21f2fac5c3bf82d2c14
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-05-06 22:54:43 +0700

    refactor:default value header middleware

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/register')) {
     const requestHeaders = new Headers(request.headers)
-    requestHeaders.set("country", request.geo?.country ?? "ID")
-    requestHeaders.set("region", request.geo?.region ?? "JI")
+    requestHeaders.set("country", request.geo?.country ?? "nCnty")
+    requestHeaders.set("region", request.geo?.region ?? "nRgin")
     requestHeaders.set("ip", request.ip ?? "noIP")
     return NextResponse.next({
         request: {
