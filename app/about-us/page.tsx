edb5e23b314d373e6163f156cd2eae472c2da9e6
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-05-06 23:14:14 +0700

    feat: add copywriting and margin top

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -8,10 +8,13 @@ export const metadata: Metadata = {
 };
 export default function Page() {
   return (
-    <main className='flex h-screen flex-col  justify-center items-center p-16'>
+    <main className='flex h-screen flex-col  justify-center items-center p-16 mt-16'>
         <div className='flex flex-col items-start gap-3.5 lg:w-[629px]'>
             <h1 className='text-black text-xl font-semibold md:text-2xl'>About IdentPix</h1>
-            <p className='text-black text-sm font-medium lg:text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim</p>
+            <p className='text-black text-sm lg:text-base'>Effortless photo search boosts productivity in various tasks. IdentPix, a photo distribution service app, streamlines processes with AI-driven facial classification technology. Additionally, we offer unlimited access, with a web app model and feature access designed to benefit both providers and users.<br/><br/>
+            We strive to be an innovative platform for photo distribution, seamlessly integrating AI technology to introduce an improved framework. Our platform is dependable, user-friendly, and tailored to meet market demands.<br/><br/>
+            Our goal is to simplify access for users through an efficient distribution flow, made possible by an accessible web app accessible via a single link. This saves time by expediting photo distribution, organizing photos by individuals, and ensuring secure privacy.
+            </p>
             <Link href={"/"}>
                 <Button className='p-0' variant="link">Learn more</Button>
             </Link>
