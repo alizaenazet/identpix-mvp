3b1a9785d8709000acb5f6cbe1eea8d0a7f05991
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-05-06 13:17:14 +0700

    feat: add user insert action

--- app/ui/register/register-form.tsx
+++ app/ui/register/register-form.tsx
@@ -3,60 +3,78 @@ import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
 import Link from 'next/link'
 import { Input } from "@/components/ui/input"
-
-export default function RegisterForm() {
+import { UserGeo } from "@/app/lib/definition";
+import { insertUser } from "@/app/lib/actions";
+import { useFormState } from 'react-dom';
+export default function RegisterForm({userGeo} : {userGeo: UserGeo}) {
     if (localStorage.getItem("subcriptionStatus") == null) {
         localStorage.setItem("subcriptionStatus", "true")
     }
-
+    
+    const initialState = { message: "", errors: {} };
+    const [state, dispatch] = useFormState(insertUser, initialState);
   return (
-    <form action="/" className="gap-y-3 md: gap-x-3 flex flex-col w-full ">
+    <form action={dispatch} className="gap-y-3 md: gap-x-3 flex flex-col w-full ">
             <div className="flex flex-1 flex-col gap-1">
                 <Label>Email</Label>
-                <Input type="email" placeholder="Email" />
+                <Input type="email" name="email" placeholder="Email" />
             </div>
         <div className="gap-y-3 md: gap-x-3 flex flex-col md:flex-row w-full">
             <div className="flex flex-1 flex-col gap-1">
                 <Label>Backround</Label>
                 <select 
-                name="" 
+                name="backround" 
                 id=""  
-                defaultValue="photographer"
+                defaultValue="0"
                 className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                 >
-                <option key="photographer" value="photographer">
+                <option key="photographer" value="0">
                     photographer
                 </option>
-                <option key="free-lancer" value="free-lancer">
+                <option key="free-lancer" value="1">
                     freelancer
                 </option>
-                <option key="student" value="student">
+                <option key="student" value="2">
                     student
                 </option>
                 </select>
             </div>
             <div className="flex flex-1 flex-col gap-1">
                 <Label>Choose plan</Label>
-                <select 
-                name="" 
+                <select
+                name="plan" 
                 id=""
-                defaultValue="individual"
+                defaultValue="1"
                 className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                 >
-                <option value="">
+                <option key="on-demand" value="0">
+                    Pas as You Go
+                </option>
+                <option key="individual" value="1">
                     individual
                 </option>
-                <option value="">
+                <option key="small-team" value="2" >
                     small team
                 </option>
-                <option value="">
+                <option key="organization" value="3">
                     organization
                 </option>
 
                 </select>
             </div>
+            <input type="text" name="ip" hidden value={userGeo.ip}  />
+            <input type="text" name="country" hidden value={userGeo.country}  />
+            <input type="text" name="region" hidden value={userGeo.region}  />
         </div>
-            <Button className="w-full" >Continue</Button>
+            <Button className="w-full" type="submit" >Continue</Button>
+            {state.errors?.ip &&
+                state.errors.ip.map((err:string) => (
+                    <p key={err}>{err}</p>
+                ))
+            }
+            {state.message && 
+                <p>{state.message}</p>
+            }
     </form>
   )
 }
